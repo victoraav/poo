4c77e2215203bd60548b32e0fdc7219b93258862
Victor <66449550+victoraav@users.noreply.github.com>
2021-08-18 16:54:12 -0300

    Add files via upload

---
+++ tabuada.java
@@ -0,0 +1,15 @@
+/*Victor Vianna - 0050016379*/
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Tabuada do 10:\n");
+    
+        for (int i=1; i<11; i++)
+        {
+        
+            int tabu = 10 * i;
+            System.out.println(tabu);
+        }
+    }
+  }
